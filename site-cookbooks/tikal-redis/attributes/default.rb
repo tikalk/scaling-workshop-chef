9fc4b7a47e7e257e2931a13b3020e26031965080
KensoDev <avi@kensodev.com>
2013-12-14 22:42:23 +0200

    Fixing redis

--- site-cookbooks/tikal-redis/attributes/default.rb
+++ site-cookbooks/tikal-redis/attributes/default.rb
@@ -4,6 +4,6 @@
 #
 # Warning: The PPA might change the version name when Chris feels like it ...
 
-default['redis']['version'] = "2:2.6.16-1chl1~#{node['lsb']['codename']}1"
+default['redis']['version'] = "2:2.8.2-1chl1~#{node['lsb']['codename']}1"
 default['redis']['apt']['uri'] = 'http://ppa.launchpad.net/chris-lea/redis-server/ubuntu'
 default['redis']['apt']['key'] = 'C7917B12'
